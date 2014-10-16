8653f0714e283ed99530c9bfaebc3c8691ca673d
Joseph Werle <joseph.werle@gmail.com>
2014-10-16 18:22:04 -0400

    init

--- README.md
+++ README.md
@@ -2,3 +2,24 @@ jsdom-repl
 ==========
 
 Simple jsdom repl
+
+## install
+
+```sh
+$ [sudo] npm i -g jsdom-repl
+```
+
+## usage
+
+```sh
+$ jsdom-repl http://google.com
+dom> console.log(location.href)
+http://www.google.com/
+dom> console.log(document.body.innerHTML)
+<script>(function(){var src='/images/nav_logo176.png';var iesg=false;document.body.onload = function(){window.n && window.n();if
+...
+```
+
+## license
+
+MIT
---
+++ index.js
@@ -0,0 +1,76 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies
+ */
+
+var repl = require('repl')
+  , dom = require('jsdom')
+  , Batch = require('batch')
+  , argv = require('minimist')(process.argv.slice(2))
+
+var exit = process.exit;
+var put = console.log.bind(console);
+var tasks = new Batch().concurrency(1);
+var runtime = null;
+var window = null;
+
+process.on('SIGINT', exit);
+
+tasks.push(function (next) {
+  var src = argv._[0];
+
+  if (null == src) {
+    return next(new Error("Missing source url or html"));
+  }
+
+  runtime = dom.env(src, argv.script || [], ready);
+
+  function ready (err, win) {
+    if (err) {
+      return next(err);
+    }
+
+    window = win;
+    next();
+  }
+});
+
+tasks.push(function (next) {
+  var r = repl.start({
+    prompt: 'dom> ',
+    input: process.stdin,
+    output: process.stdout,
+    useGlobal: false,
+    ignoreUndefined: true,
+    eval: oneval
+  });
+
+  r.context = window;
+  window.console = console;
+
+
+  r.on('exit', function () {
+    put()
+    put("bye : ]");
+    exit(0)
+  });
+
+  function oneval (src, context, filename, done) {
+    src = src.substr(1)
+    src = src.substr(0, src.length - 1);
+    try {
+      var res = context.eval(src);
+      done(null, res);
+    } catch (e) {
+      done(e);
+    }
+  }
+});
+
+tasks.end(function (err) {
+  if (err) {
+    console.error(err.message);
+    exit(1);
+  }
+});
